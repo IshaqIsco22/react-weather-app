d6eba3e347d1fb5dfafcc6bc5e9b12cccea764e2
IshaqIsco22 <ishanpkd786@gmail.com>
2023-03-09 01:14:43 +0530

    weather app

---
+++ src/apis.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a8a990e546msh15d5a1c4fbaf93cp171e88jsn06f066f29b4e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "a43f10d30284ab985507c25ff6b4e9aa";
