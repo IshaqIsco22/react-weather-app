40866759000f15bbf6be92a85285ba5458ea1384
IshaqIsco22 <ishanpkd786@gmail.com>
2023-06-07 11:15:55 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import CurrentWeather from "./components/current weather/currentWeather";
 import { useState } from "react";
 import { WEATHER_API_KEY, WEATHER_API_URL } from "./apis";
 import Forecast from "./components/forecast/forecast";
+import Icon from "./imgs/weather2.png";
+
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -34,6 +36,20 @@ function App() {
   console.log(forecast);
   return (
     <div className="container">
+      <div style={{ paddingBottom: "10px", display: "inline-flex" }}>
+        <img
+          src={Icon}
+          alt="logo"
+          style={{
+            marginRight: "10px",
+            height: "50px",
+            width: "50px",
+            display: "flex",
+            paddingTop: "5px",
+          }}
+        />
+        <h1 className="h1">WEATHER APP</h1>
+      </div>
       <Search onSearchData={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
